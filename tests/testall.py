f5ace7ea8badb2401190e4845fb0216c7a80891e
Thomas Leonard <talex5@gmail.com>
2011-04-29 16:49:13 +0100

    Allow specifying which unit-tests to run
    e.g. 0test ../0release.xml -- testrelease.TestRelease.testBinaryRelease

--- tests/testall.py
+++ tests/testall.py
@@ -11,18 +11,22 @@ my_dir = os.path.dirname(sys.argv[0])
 if not my_dir:
 	my_dir = os.getcwd()
 
-sys.argv.append('-v')
+testLoader = unittest.TestLoader()
 
-suite_names = [f[:-3] for f in os.listdir(my_dir)
-		if f.startswith('test') and f.endswith('.py')]
-suite_names.remove('testall')
-suite_names.sort()
+if len(sys.argv) > 1:
+	alltests = testLoader.loadTestsFromNames(sys.argv[1:])
+else:
+	alltests = unittest.TestSuite()
 
-alltests = unittest.TestSuite()
+	suite_names = [f[:-3] for f in os.listdir(my_dir)
+			if f.startswith('test') and f.endswith('.py')]
+	suite_names.remove('testall')
+	suite_names.sort()
 
-for name in suite_names:
-	m = __import__(name, globals(), locals(), [])
-	alltests.addTest(m.suite)
+	for name in suite_names:
+		m = __import__(name, globals(), locals(), [])
+		t = testLoader.loadTestsFromModule(m)
+		alltests.addTest(t)
 
 a = unittest.TextTestRunner(verbosity=2).run(alltests)
 
